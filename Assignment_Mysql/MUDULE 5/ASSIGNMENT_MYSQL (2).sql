4dd0496e6afda9d32d9d30a886af6595dd6a781d
jasanichirag <jasanichirag1999@gmail.com>
2023-03-01 10:10:05 +0530

    assignment submit

--- Assignment_Mysql/MUDULE 5/ASSIGNMENT_MYSQL (2).sql
+++ Assignment_Mysql/MUDULE 5/ASSIGNMENT_MYSQL (2).sql
@@ -1,9 +1,15 @@
 --  <-------------- CREATE DATABASE :-  --------------->
+
 create database assignment;
+
 -- <----------------  USE DATABASE :  ----------------->
+
 use assignment;
+
 --  <--  1) Create Table Name : Student and Exam :-  -->
+
 -- <----------  1) CREATE TABLE STDUENT :-  ----------->
+
 create table
     stduent(
         roll_NO int auto_increment,
@@ -22,52 +28,68 @@ create table
         P_code varchar(8),
         FOREIGN KEY (roll_no) references stduent(roll_No)
     );
+
 --  <-------- INSERT DATA INTO STDUENT TABLE :-  ----->
+
 insert INTO stduent (NAME,BRANCH) VALUES("JAY","COMPUTER SCIENCE");
+
 INSERT INTO
     STDUENT (NAME, BRANCH)
 VALUES (
         "SUHANI",
         "ELECTRONIC AND COM"
     );
+
 INSERT INTO
     STDUENT (NAME, BRANCH)
 VALUES ("KRITI", "ELECTONIC AND COM");
 
 --  <------------ INSERT DATA INTO EXAM TABLE :-  ------------------->
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES(1, "CS11", 50, "C8");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES (1, "CS12", 60, "C8");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES (2, "EC101", 66, "EC");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 values(2, "EC102", 70, "EC");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 values(3, "EC101", 45, "EC");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 values(3, "EC102", 50, "EC");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES(1, "CS11", 50, "CS");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES(1, "CS12", 60, "CS");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES(2, "EC101", 66, "EC");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES(2, "EC102", 70, "EC");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES (3, "EC101", 45, "EC");
+
 INSERT INTO
     EXAM (ROLL_NO, S_CODE, MARK, P_CODE)
 VALUES(3, "EC102", 50, "EC");
@@ -218,6 +240,7 @@ values (
 --  3. Create table given below: Employee and Incentive :-
 
 --  <---------------   1) create table Employee :  ------------------>
+
 CREATE TABLE
     employee(
         emp_id int AUTO_INCREMENT,
@@ -335,40 +358,97 @@ VALUES (
 
 --  <------------ insert data into table incentive :  --------------->
 
-INSERT INTO
-    incentive
-VALUES (1, "2013-02-01", 5000),
- (2, "2013-02-01", 3000),
- (3, "2013-02-01", 4000),
- (1, "2013-01-01", 4500),
- (2, "2013-01-01", 3500),
- (5, "2012-01-01", 2000),
- (5, "2015-05-01", 1500),
- (4, "2014-02-01", 2500),
- (6, "2013-01-01", 1000);
+INSERT INTO incentive
+VALUES (1, "2013-02-01", 5000), (2, "2013-02-01", 3000), (3, "2013-02-01", 4000), (1, "2013-01-01", 4500), (2, "2013-01-01", 3500), (5, "2012-01-01", 2000), (5, "2015-05-01", 1500), (4, "2014-02-01", 2500), (6, "2013-01-01", 1000);
 
 -- <-----------------------------  ANS  :  -------------------------->
 
 --  <--  a) Get First_Name from employee table using Tom name “Employee Name”.  -->
+
 SELECT * FROM employee WHERE first_name="tom";
 
 --  <--   b) Get FIRST_NAME, Joining Date, and Salary from employee table. : -->
+
 select first_name,joining_date,salary FROM employee ;
 
 --   <--  c) Get all employee details from the employee table order by First_Name Ascending and Salary descending ? -->
+
 select * from employee ORDER BY first_name,salary DESC;
 
 --  <--  d) Get employee details from employee table whose first name contains ‘J’.  -->
+
 select * FROM employee WHERE first_name LIKE "j%";
 
 -- <--  e) Get department wise maximum salary from employee table order by salary ascending ?  -->
-SELECT *, MAX(salary) FROM employee GROUP BY department ORDER BY salary;
+
+SELECT *, MAX(salary)
+FROM employee
+GROUP BY department
+ORDER BY salary;
 
 -- <--  f) Select first_name, incentive amount from employee and incentives table for those employees who have incentives and incentive amount  greater than 3000;  -->
-SELECT employee.*, incentive.*,SUM(incentive.incentive_amount) AS incentive FROM employee LEFT JOIN incentive ON employee.emp_id = incentive.emp_ref_id WHERE incentive.incentive_amount > 3000 GROUP BY employee.emp_id;
+
+SELECT
+    employee.*,
+    incentive.*,
+    SUM(incentive.incentive_amount) AS incentive
+FROM employee
+    LEFT JOIN incentive ON employee.emp_id = incentive.emp_ref_id
+WHERE
+    incentive.incentive_amount > 3000
+GROUP BY employee.emp_id;
 
 --  <--  g) Create After Insert trigger on Employee table which insert records in view table;  -->
- CREATE TABLE BACKUP_EMPLOYEE(EMP_ID INT,FIRST_NAME VARCHAR(10),LAST_NAME VARCHAR(10),SALARY INT,JOINING_DATE DATETIME,DEPARTMENT VARCHAR(10));
+--  <--------- create backup table of employee  -------------->
+CREATE TABLE
+    BACKUP_Emp(
+        emp_id int,
+        first_name VARCHAR(10),
+        last_name VARCHAR(10),
+        salary INT,
+        joining_date DATETIME,
+        department VARCHAR(10)
+    );
+    --  <-------------- INSERT into employee table :  -------->
+insert into
+    employee(
+        first_name,
+        last_name,
+        salary,
+        joining_date,
+        department
+    )
+VALUES (
+        "rohan",
+        "pinto",
+        654000,
+        "2013-02-01 12:00:00",
+        "insurance"
+    );
+--  <------------  trigger  --------------->
+
+CREATE TRIGGER EMP_BACK 
+	after
+	INSERT on employee for each row
+	insert into
+	    backup_emp(
+	        emp_id,
+	        first_name,
+	        last_name,
+	        salary,
+	        joining_date,
+	        department
+	    )
+	values
+	(
+	        new.emp_id,
+	        new.first_name,
+	        new.last_name,
+	        new.salary,
+	        new.joining_date,
+	        new.department
+	    );
+
 
 
 --   <--   4. Create table given below: Salesperson and Customer :-  -->
@@ -387,8 +467,7 @@ CREATE table
 --   <------------  INSERT DATA INTO SALESPERSON TABLE :-  ---------->
 
 INSERT INTO SALESPERSON
-VALUES
-(1001, "KEYUR", "SURAT", 0.35), (
+VALUES (1001, "KEYUR", "SURAT", 0.35), (
         1002,
         "AKSHAY",
         "BHAVNAGAR",
@@ -401,8 +480,7 @@ VALUES
     ), (1004, "VIVEK", "RAJKOT", 0.34), (1005, "JAY", "MAHUVA", 0.35), (1006, "MAYUR", "TALAJA", 0.30);
 
 INSERT INTO SALESPERSON
-VALUES
-(1007, "MIHIR", "SURAT", 0.1), (1008, "KETAN", "BHUJ", 0.09), (1009, "VAIDIK", "NADIYAD", 0.05), (
+VALUES (1007, "MIHIR", "SURAT", 0.1), (1008, "KETAN", "BHUJ", 0.09), (1009, "VAIDIK", "NADIYAD", 0.05), (
         1010,
         "GAURANG",
         "BHAVNAGAR",
@@ -425,8 +503,7 @@ CREATE Table
 --   <----------  2) INSERT DATA INTO CUSTOMER TABLE :-  ------------>
 
 INSERT INTO CUSTOMER
-VALUES
-(
+VALUES (
         201,
         "RITESH",
         "SURAT",
@@ -471,8 +548,7 @@ VALUES
     ), (209, "RAJ", "SURAT", 300, 1001);
 
 INSERT INTO CUSTOMER
-VALUES
-(210, "RAM", "SURAT", 200, 1007), (
+VALUES (210, "RAM", "SURAT", 200, 1007), (
         211,
         "VIRAMDEV",
         "BHUJ",
@@ -495,23 +571,51 @@ VALUES
 --  <------  Retrieve the below data from above table :-  ----------->
 
 --   <-------   a) All orders for more than $1000. :-   ------------->
+
 --  <--------------  SHOW SALESPERSON TABLE  ------------------->
+
 SELECT * FROM SALESPERSON;
+
 --   <------------  SHOW CUSTOMER TABLE    ------------------->
+
 SELECT * FROM CUSTOMER;
+
 --  <------------  SHOW SALESPERSON & CUSTOMER TABLE  ---------->
-SELECT    CUSTOMER.*, SALESPERSON.* FROM CUSTOMER INNER JOIN SALESPERSON ON CUSTOMER.S_NO = SALESPERSON.S_NO;
+
+SELECT
+    CUSTOMER.*,
+    SALESPERSON.*
+FROM CUSTOMER
+    INNER JOIN SALESPERSON ON CUSTOMER.S_NO = SALESPERSON.S_NO;
 
 --  <--  b) Names and cities of all alespeople in SURAT with commission above 0.12 :-  -->
-SELECT SALESPERSON.S_NAME AS SALESMAN, SALESPERSON.COMMISSION,CUSTOMER.CNAME AS CUSTOMER,CUSTOMER.CITY,CUSTOMER.RATING FROM SALESPERSON LEFT JOIN CUSTOMER ON SALESPERSON.S_NO = CUSTOMER.S_NO WHERE SALESPERSON.COMMISSION > 0.12;
+
+SELECT
+    SALESPERSON.S_NAME AS SALESMAN,
+    SALESPERSON.COMMISSION,
+    CUSTOMER.CNAME AS CUSTOMER,
+    CUSTOMER.CITY,
+    CUSTOMER.RATING
+FROM SALESPERSON
+    LEFT JOIN CUSTOMER ON SALESPERSON.S_NO = CUSTOMER.S_NO
+WHERE
+    SALESPERSON.COMMISSION > 0.12;
 
 --  <--  c) All salespeople either in SURAT  or in BHAVNAGAR  :-  -->
+
 SELECT * FROM SALESPERSON WHERE CITY="SURAT" OR CITY="BHAVNAGAR";
 
 -- <--  d) All salespeople with commission between 0.05 and 0.3 (Boundary values should be excluded). :-  -->
-SELECT * FROM SALESPERSON WHERE COMMISSION < 0.3 AND COMMISSION > 0.05 ORDER BY COMMISSION;
+
+SELECT *
+FROM SALESPERSON
+WHERE
+    COMMISSION < 0.3
+    AND COMMISSION > 0.05
+ORDER BY COMMISSION;
 
 -- <--  e) All customers excluding those with rating <= 100 unless they are located in Rome : -->
+
 SELECT * FROM CUSTOMER WHERE RATING >=100 AND CITY="SURAT";
 
 --   <<----------------   THANKING  ----  YOU  --------------------->>
